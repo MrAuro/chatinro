b6475906ebe7fe1c2196d2654b55e6619afdcc9d
Auro <35087590+MrAuro@users.noreply.github.com>
2021-04-04 23:57:00 -0400

    Add project files.

---
+++ chatinro/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿
+namespace chatinro
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
+			((System.ComponentModel.ISupportInitialize)(this.webView21)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// webView21
+			// 
+			this.webView21.CreationProperties = null;
+			this.webView21.DefaultBackgroundColor = System.Drawing.Color.White;
+			this.webView21.Location = new System.Drawing.Point(12, 12);
+			this.webView21.Name = "webView21";
+			this.webView21.Size = new System.Drawing.Size(867, 871);
+			this.webView21.Source = new System.Uri("https://www.twitch.tv/popout/pajlada/chat", System.UriKind.Absolute);
+			this.webView21.TabIndex = 0;
+			this.webView21.ZoomFactor = 1D;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			this.ClientSize = new System.Drawing.Size(891, 895);
+			this.Controls.Add(this.webView21);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "Form1";
+			this.Text = "chatinro";
+			((System.ComponentModel.ISupportInitialize)(this.webView21)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
+	}
+}
+
